643ef270e64b06a73c771de122dcd17686a7de81
bozo.kopic <bozo@kopic.xyz>
2024-01-30 20:59:12 +0100

    init

---
+++ src_doit/__init__.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+
+from hat.doit import common
+from hat.doit.docs import (build_sphinx,
+                           build_pdoc)
+from hat.doit.py import (get_task_build_wheel,
+                         get_task_run_pytest,
+                         get_task_create_pip_requirements,
+                         run_flake8)
+
+
+__all__ = ['task_clean_all',
+           'task_build',
+           'task_check',
+           'task_test',
+           'task_docs',
+           'task_json_schema_repo',
+           'task_pip_requirements']
+
+
+build_dir = Path('build')
+docs_dir = Path('docs')
+pytest_dir = Path('src_pytest')
+schemas_json_dir = Path('schemas_json')
+src_py_dir = Path('src_py')
+
+build_py_dir = build_dir / 'py'
+build_docs_dir = build_dir / 'docs'
+
+json_schema_repo_path = src_py_dir / 'hat/controller/json_schema_repo.json'
+
+
+def task_clean_all():
+    """Clean all"""
+    return {'actions': [(common.rm_rf, [
+        build_dir,
+        json_schema_repo_path])]}
+
+
+def task_build():
+    """Build"""
+    return get_task_build_wheel(src_dir=src_py_dir,
+                                build_dir=build_py_dir,
+                                task_dep=['json_schema_repo'])
+
+
+def task_check():
+    """Check with flake8"""
+    return {'actions': [(run_flake8, [src_py_dir]),
+                        (run_flake8, [pytest_dir])]}
+
+
+def task_test():
+    """Test"""
+    return get_task_run_pytest(task_dep=['json_schema_repo'])
+
+
+def task_docs():
+    """Docs"""
+
+    def build():
+        build_sphinx(src_dir=docs_dir,
+                     dst_dir=build_docs_dir,
+                     project='hat-controller',
+                     extensions=['sphinxcontrib.programoutput'])
+        build_pdoc(module='hat.controller',
+                   dst_dir=build_docs_dir / 'py_api')
+
+    return {'actions': [build],
+            'task_dep': ['json_schema_repo']}
+
+
+def task_json_schema_repo():
+    """Generate JSON Schema Repository"""
+    return common.get_task_json_schema_repo(schemas_json_dir.rglob('*.yaml'),
+                                            json_schema_repo_path)
+
+
+def task_pip_requirements():
+    """Create pip requirements"""
+    return get_task_create_pip_requirements()
---
+++ src_py/hat/controller/environment.py
@@ -0,0 +1,145 @@
+from collections.abc import Collection
+import asyncio
+import logging
+import typing
+
+from hat import aio
+from hat import json
+from hat import duktape
+
+from hat.controller import common
+
+
+mlog = logging.getLogger(__name__)
+
+
+class UnitProxy(typing.NamedTuple):
+    unit: common.Unit
+    info: common.UnitInfo
+
+
+class Environment(aio.Resource):
+
+    def __init__(self,
+                 environment_conf: json.Data,
+                 proxies: Collection[UnitProxy],
+                 trigger_queue_size: int = 4096):
+        self._loop = asyncio.get_running_loop()
+        self._executor = aio.Executor(log_exceptions=False)
+        self._trigger_queue = aio.Queue(trigger_queue_size)
+        self._proxies = {proxy.info.name: proxy for proxy in proxies}
+        self._last_trigger = None
+        self._init_code = environment_conf['init']
+        self._action_confs = {action_conf['name']: action_conf
+                              for action_conf in environment_conf['actions']}
+
+        self.async_group.spawn(self._run_loop)
+
+    @property
+    def async_group(self) -> aio.Group:
+        return self._executor.async_group
+
+    async def process_trigger(self, trigger: common.Trigger):
+        await self._trigger_queue.put(trigger)
+
+    async def _run_loop(self):
+        try:
+            interpreter = await self._executor.spawn(duktape.Interpreter)
+
+            infos = (proxy.info for proxy in self._proxies.values())
+            await self._executor.spawn(_ext_init_api, interpreter, infos,
+                                       self._ext_call)
+
+            actions = {}
+            for action_name, action_conf in self._action_confs.items():
+                actions[action_name] = await self._executor.spawn(
+                    _ext_create_action, interpreter, action_conf['code'])
+
+            await self._executor.spawn(interpreter.eval, self._init_code)
+
+            while True:
+                self._last_trigger = await self._trigger_queue.get()
+
+                for action_name in self._get_matching_action_names():
+                    action = actions[action_name]
+                    await self._executor.spawn(action)
+
+        except Exception as e:
+            mlog.error('run loop error: %s', e, exc_info=e)
+
+        finally:
+            self.close()
+            self._trigger_queue.close()
+
+    async def _call(self, unit_name, function, args):
+        proxy = self._proxies[unit_name]
+        return await aio.call(proxy.unit.call, function, args,
+                              self._last_trigger)
+
+    def _ext_call(self, unit_name, function, args):
+        coro = self._call(unit_name, function, args)
+        future = asyncio.run_coroutine_threadsafe(coro, self._loop)
+        return future.result()
+
+    def _get_matching_action_names(self):
+        for action_name, action_conf in self._action_confs.items():
+            for trigger_conf in action_conf['triggers']:
+                if _match_trigger(self._last_trigger, trigger_conf['type'],
+                                  trigger_conf['name']):
+                    yield action_name
+                    break
+
+
+def _ext_init_api(interpreter, infos, call_cb):
+
+    def encode(x):
+        if isinstance(x, str):
+            return x
+
+        elements = (f"'k': {encode(v)}" for k, v in x.items())
+        return f"{{{', '.join(elements)}}}"
+
+    api_dict = {}
+    for info in infos:
+        unit_api_dict = {}
+
+        for function in info.functions:
+            segments = function.split('.')
+            parent = unit_api_dict
+
+            for segment in segments[:-1]:
+                if segment not in parent:
+                    parent[segment] = {}
+
+                parent = parent[segment]
+
+            parent[segments[-1]] = (
+                f"function () {{ "
+                f"return f('{info.name}', '{function}', arguments); "
+                f"}}")
+
+        api_dict[info.name] = unit_api_dict
+
+    units = encode(api_dict)
+    init_fn = interpreter.eval(f"var units; "
+                               f"function (f) {{ units = {units}; }}")
+    init_fn(call_cb)
+
+
+def _ext_create_action(interpreter, code):
+    return interpreter.eval(f"function () {{ {code} }}")
+
+
+def _match_trigger(trigger, type_query, name_query):
+    return (_match_query(trigger.type, type_query) and
+            _match_query(trigger.name, name_query))
+
+
+def _match_query(value, query):
+    if query == '*':
+        return True
+
+    if query.endswith('/*'):
+        return value.startswith(query[:-1]) or value == query[:-2]
+
+    return value == query
