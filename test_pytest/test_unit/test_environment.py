ebbf4722b2ec2d257cb4e2fd6d01a9dc4fcdfd38
Jakov Krstulovic Opara <jakov.krstulovic@koncar.hr>
2024-04-09 22:29:10 +0200

    test unit environment - trigger subscription

--- test_pytest/test_unit/test_environment.py
+++ test_pytest/test_unit/test_environment.py
@@ -1,4 +1,5 @@
 import asyncio
+import pytest
 
 from hat import aio
 
@@ -111,8 +112,7 @@ async def test_action():
 
     env = hat.controller.environment.Environment(
         environment_conf=env_conf,
-        proxies=[unit_proxy],
-        trigger_queue_size=100)
+        proxies=[unit_proxy])
 
     await asyncio.sleep(0.01)
     assert unit_call_queue.empty()
@@ -122,10 +122,115 @@ async def test_action():
                              data={'test_data': 123})
     await env.process_trigger(trigger)
 
-    function, args, trigger_res = await unit_call_queue.get()
+    function, args, triggered_by = await unit_call_queue.get()
 
     assert function == unit_fn
     assert args == ['x', 'y', 123]
-    assert trigger_res == trigger
+    assert triggered_by == trigger
+
+    await env.async_close()
+
+
+@pytest.mark.parametrize('trigger_type, trigger_name, triggered_actions', [
+    ('test/a', 'x/a', ['a1', 'a3', 'a4', 'a5', 'a7']),
+    ('test/a/b', 'x/a', ['a3', 'a5', 'a7']),
+    ('test', 'x/a', ['a3', 'a5', 'a7']),
+    ('test/a', 'x/a/b/c/d/1', ['a3', 'a4']),
+    ('test/a', 'x/bla/123', ['a3', 'a4']),
+    ('test/a', 'x', ['a3', 'a4']),
+    ('test/b', 'y/b', ['a1', 'a3', 'a4', 'a5']),
+    ('test/b', 'y/bla', ['a3', 'a4']),
+    ('test/b', 'y', ['a3', 'a4']),
+    ('test/b', 'z/bla', ['a3']),
+    ('test/c', 'z/c', ['a1', 'a3']),
+    ('test/a', 'bla', ['a3']),
+    ('bla', 'x/a', ['a3', 'a5']),
+    ('bla/123', 'y/b', ['a3', 'a5']),
+    ('test/c', 'z/c', ['a1', 'a3']),
+    ('bla', 'bla/123', ['a3']),
+    ('', '', ['a3']),
+    ('test2/a', 'x/d', ['a2', 'a3'])])
+async def test_trigger_subscription(trigger_type, trigger_name,
+                                    triggered_actions):
+    unit_call_queue = aio.Queue()
+
+    def on_unit_call(function, args, trigger):
+        unit_call_queue.put_nowait((function, args, trigger))
+
+    unit = MockUnit(call_cb=on_unit_call)
+    unit_name = 'u1'
+    unit_fn = 'f1'
+    unit_proxy = hat.controller.environment.UnitProxy(
+        unit=unit,
+        info=common.UnitInfo(
+            name=unit_name,
+            functions={unit_fn},
+            create=MockUnit,
+            json_schema_id=None,
+            json_schema_repo=None))
+
+    env_conf = {
+        'name': 'env1',
+        'init_code': "",
+        'actions': [
+            {'name': 'a1',
+             'triggers': [{'type': 'test/a',
+                           'name': 'x/a'},
+                          {'type': 'test/b',
+                           'name': 'y/b'},
+                          {'type': 'test/c',
+                           'name': 'z/c'}],
+             'code': f"units.{unit_name}.{unit_fn}('a1')"},
+            {'name': 'a2',
+             'triggers': [{'type': 'test2/a',
+                           'name': 'x/d'}],
+             'code': f"units.{unit_name}.{unit_fn}('a2')"},
+            {'name': 'a3',
+             'triggers': [{'type': '*',
+                           'name': '*'}],
+             'code': f"units.{unit_name}.{unit_fn}('a3')"},
+            {'name': 'a4',
+             'triggers': [{'type': 'test/a',
+                           'name': 'x/*'},
+                          {'type': 'test/b',
+                           'name': 'y/*'}],
+             'code': f"units.{unit_name}.{unit_fn}('a4')"},
+            {'name': 'a5',
+             'triggers': [{'type': '*',
+                           'name': 'x/a'},
+                          {'type': '*',
+                           'name': 'y/b'}],
+             'code': f"units.{unit_name}.{unit_fn}('a5')"},
+            {'name': 'a6',
+             'triggers': [],
+             'code': f"units.{unit_name}.{unit_fn}('a6')"},
+            {'name': 'a7',
+             'triggers': [{'type': 'test/*',
+                           'name': 'x/a'}],
+             'code': f"units.{unit_name}.{unit_fn}('a7')"}]}
+
+    env = hat.controller.environment.Environment(
+        environment_conf=env_conf,
+        proxies=[unit_proxy])
+
+    await asyncio.sleep(0.01)
+    assert unit_call_queue.empty()
+
+    trigger = common.Trigger(type=trigger_type,
+                             name=trigger_name,
+                             data={'test_data': 123})
+    await env.process_trigger(trigger)
+
+    triggered_actions_res = set()
+
+    for _ in range(len(triggered_actions)):
+        _, args, triggered_by = await unit_call_queue.get()
+        assert triggered_by == trigger
+        triggered_actions_res.add(args[0])
+
+    await asyncio.sleep(0.01)
+    assert unit_call_queue.empty()
+
+    assert triggered_actions_res == set(triggered_actions)
 
     await env.async_close()
