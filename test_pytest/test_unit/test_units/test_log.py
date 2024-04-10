a1bb58b33c8462d6274c5116c49d81568d0de2a8
Jakov Krstulovic Opara <jakov.krstulovic@koncar.hr>
2024-04-10 15:28:07 +0200

    test unit units log

---
+++ test_pytest/test_unit/test_units/test_log.py
@@ -0,0 +1,126 @@
+import logging
+import pytest
+
+from hat import aio
+
+from hat.controller.units.log import info
+
+
+def test_conf():
+    conf = {'logger': 'xyz'}
+    info.json_schema_repo.validate(info.json_schema_id, conf)
+
+
+async def test_create():
+    conf = {'logger': 'xyz'}
+
+    unit = await aio.call(info.create, conf, None)
+
+    assert unit.is_open
+
+    unit.close()
+    await unit.wait_closing()
+    assert unit.is_closing
+    await unit.wait_closed()
+    assert unit.is_closed
+
+
+@pytest.mark.parametrize('level, message', [
+    ('INFO', 'this is info message'),
+    ('DEBUG', 'this is debug message'),
+    ('WARNING', 'this is warning message'),
+    ('ERROR', 'this is error message')])
+async def test_log(caplog, level, message):
+    logger = 'xyz'
+    conf = {'logger': logger}
+
+    caplog.set_level(logging.getLevelName(level), logger)
+
+    unit = await aio.call(info.create, conf, None)
+
+    result = await aio.call(unit.call, 'log', [level, message], None)
+    assert result is None
+
+    assert len(caplog.records) == 1
+    for record in caplog.records:
+        assert record.name == logger
+        assert record.msg == message
+        assert logging.getLevelName(record.levelno) == level
+
+    await unit.async_close()
+
+
+async def test_log_multi_messages(caplog):
+    logger = 'xyz'
+    conf = {'logger': logger}
+    msg_count = 10
+    level = 'INFO'
+
+    caplog.set_level(logging.getLevelName(level), logger)
+
+    unit = await aio.call(info.create, conf, None)
+
+    messages = [f"msg {i}" for i in range(msg_count)]
+
+    for message in messages:
+        result = await aio.call(unit.call, 'log', [level, message], None)
+        assert result is None
+
+    assert len(caplog.records) == msg_count
+    for message, record in zip(messages, caplog.records):
+        assert record.name == logger
+        assert record.msg == message
+        assert logging.getLevelName(record.levelno) == level
+
+    await unit.async_close()
+
+
+@pytest.mark.parametrize('level, message', [
+    ('INFO', 'this is info message'),
+    ('DEBUG', 'this is debug message'),
+    ('WARNING', 'this is warning message'),
+    ('ERROR', 'this is error message')])
+async def test_level_functions(caplog, level, message):
+    logger = 'xyz'
+    conf = {'logger': logger}
+
+    caplog.set_level(logging.getLevelName(level), logger)
+
+    unit = await aio.call(info.create, conf, None)
+
+    result = await aio.call(unit.call, level.lower(), [message], None)
+    assert result is None
+
+    assert len(caplog.records) == 1
+    for record in caplog.records:
+        assert record.name == logger
+        assert record.msg == message
+        assert logging.getLevelName(record.levelno) == level
+
+    await unit.async_close()
+
+
+async def test_log_invalid_level():
+    conf = {'logger': 'xyz'}
+
+    unit = await aio.call(info.create, conf, None)
+
+    with pytest.raises(Exception):
+        await aio.call(unit.call, 'log', ['INVALID', 'abc'], None)
+
+    assert unit.is_open
+
+    await unit.async_close()
+
+
+async def test_invalid_function():
+    conf = {'logger': 'xyz'}
+
+    unit = await aio.call(info.create, conf, None)
+
+    with pytest.raises(Exception):
+        await aio.call(unit.call, 'invalid', ['abc'], None)
+
+    assert unit.is_open
+
+    await unit.async_close()
